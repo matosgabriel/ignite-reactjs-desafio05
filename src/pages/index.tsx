108d1a4dbbb225018ba70b2e48564c8953872b1d
matosgabriel <gabriel.ifsal@gmail.com>
2022-04-11 01:56:40 -0300

    Applying the posts pagination using fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,16 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
-import Prismic from '@prismicio/client'; // prismic
+import Prismic from '@prismicio/client'; // prismic client
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { useEffect, useState } from 'react';
+import { DefaultClient } from '@prismicio/client/types/client';
 import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -25,6 +27,7 @@ interface Post {
 interface PostPagination {
   next_page: string;
   results: Post[];
+  prismicClient: DefaultClient;
 }
 
 interface HomeProps {
@@ -32,49 +35,70 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [nextPage, setNextPage] = useState<string | null>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    setPosts(postsPagination.results);
+    setNextPage(postsPagination.next_page);
+  }, [postsPagination.next_page, postsPagination.results]);
+
+  async function handleLoadMorePosts(): Promise<void> {
+    const data = (await (await fetch(nextPage)).json()) as PostPagination;
+
+    setPosts(currentValue => [...currentValue, ...data.results]);
+    setNextPage(data.next_page);
+  }
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <div className={styles.container}>
-        <Header />
+        <main className={styles.content}>
+          <Header />
 
-        <ul>
-          {postsPagination.results.map(post => {
-            return (
-              <li className={styles.post} key={post.uid}>
-                <Link href="/">
-                  <a>
-                    <strong>{post.data.title}</strong>
-                  </a>
-                </Link>
-                <p>{post.data.subtitle}</p>
+          <ul>
+            {posts.map(post => {
+              return (
+                <li className={styles.post} key={post.uid}>
+                  <Link href="/">
+                    <a>
+                      <strong>{post.data.title}</strong>
+                    </a>
+                  </Link>
+                  <p>{post.data.subtitle}</p>
 
-                <section>
-                  <div>
-                    <FiCalendar />
-                    <span>
-                      {format(
-                        new Date(post.first_publication_date),
-                        'dd MMM yyyy',
-                        {
-                          locale: ptBR,
-                        }
-                      )}
-                    </span>
-                  </div>
-                  <div>
-                    <FiUser />
-                    <span>{post.data.author}</span>
-                  </div>
-                </section>
-              </li>
-            );
-          })}
-        </ul>
+                  <section>
+                    <div>
+                      <FiCalendar />
+                      <span>
+                        {format(
+                          new Date(post.first_publication_date),
+                          'dd MMM yyyy',
+                          {
+                            locale: ptBR,
+                          }
+                        )}
+                      </span>
+                    </div>
+                    <div>
+                      <FiUser />
+                      <span>{post.data.author}</span>
+                    </div>
+                  </section>
+                </li>
+              );
+            })}
+          </ul>
 
-        <button type="button">Carregar mais posts</button>
+          {nextPage !== null && (
+            <button type="button" onClick={() => handleLoadMorePosts()}>
+              Carregar mais posts
+            </button>
+          )}
+        </main>
       </div>
     </>
   );
@@ -86,6 +110,7 @@ export const getStaticProps: GetStaticProps = async () => {
     Prismic.Predicates.at('document.type', 'posts'),
     {
       fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 5,
     }
   );
 
